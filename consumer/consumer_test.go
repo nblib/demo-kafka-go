8e08612c1ddd37c721e50c5c1ac16cccebb61e14
hewe <272489473@qq.com>
2017-12-05 10:15:21 +0800

    add group consumer

--- consumer/consumer_test.go
+++ consumer/consumer_test.go
@@ -4,8 +4,15 @@ import (
 	"testing"
 	"github.com/Shopify/sarama"
 	"fmt"
+	"os"
+	"os/signal"
+	"log"
+	"github.com/bsm/sarama-cluster"
 )
 
+/*
+普通消费者
+ */
 func TestConsumer(t *testing.T) {
 	//配置
 	config := sarama.NewConfig()
@@ -36,4 +43,57 @@ func TestConsumer(t *testing.T) {
 			fmt.Println(err.Err)
 		}
 	}
+}
+
+/**
+分组的消费者
+ */
+func TestConsumerGroup(t *testing.T) {
+	// init (custom) config, enable errors and notifications
+	config := cluster.NewConfig()
+	config.Consumer.Return.Errors = true
+	config.Group.Return.Notifications = true
+
+	// init consumer
+	brokers := []string{"10.169.0.214:9092"}
+	topics := []string{"logstash_test"}
+	consumer, err := cluster.NewConsumer(brokers, "my-consumer-group", topics, config)
+	if err != nil {
+		panic(err)
+	}
+	defer consumer.Close()
+
+	consumer.MarkPartitionOffset()
+
+	// trap SIGINT to trigger a shutdown.
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, os.Interrupt)
+
+	// consume errors
+	go func() {
+		for err := range consumer.Errors() {
+			log.Printf("Error: %s\n", err.Error())
+		}
+	}()
+
+	// consume notifications
+	go func() {
+		for ntf := range consumer.Notifications() {
+			log.Printf("Rebalanced: %+v\n", ntf)
+		}
+	}()
+
+	// consume messages, watch signals
+	for {
+		select {
+		case msg, ok := <-consumer.Messages():
+			if ok {
+				fmt.Fprintf(os.Stdout, "%s/%d/%d\t%s\t%s\n", msg.Topic, msg.Partition, msg.Offset, msg.Key, msg.Value)
+				consumer.MarkOffset(msg, "") // mark message as processed
+			}
+		case <-signals:
+			return
+		}
+	}
+
 }
